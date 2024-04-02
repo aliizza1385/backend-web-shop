488275c17a98350f7b5b94e94c586979813a8fbb
alireza <aliizza1385@gmail.com>
2024-04-02 02:45:49 -0700

    I create search in payment

--- admin/Payments/route.py
+++ admin/Payments/route.py
@@ -3,51 +3,66 @@ from .models import Payment
 from flask_bcrypt import generate_password_hash
 from werkzeug.exceptions import BadRequest
 from initialize import db
-
+import ast
 
 blueprint = Blueprint('payment', __name__)
 
 
-def search_payment():
-    # Get the search query from the request
-    search_query = request.args.get('name', '')
+# def search_payment():
+#     # Get the search query from the request
+#     search_query = request.args.get('name', '')
 
-    # Query the database to filter payments based on the search query
-    filtered_payments = Payment.query.filter(Payment.order_id.ilike(f'%{search_query}%')).all()
+#     # Query the database to filter payments based on the search query
+#     filtered_payments = Payment.query.filter(Payment.order_id.ilike(f'%{search_query}%')).all()
 
-    # Convert the filtered payments to a list of dictionaries
-    for payment in filtered_payments:
-        result = [
-            {
-                "id": payment.id,
-                "order_id": payment.order_id,
-                "payment_method": payment.payment_method,
-                "amount": payment.amount,
-                # "payment_date": payment.payment_date.strftime('%Y-%m-%d')
-            }
-        ]
+#     # Convert the filtered payments to a list of dictionaries
+#     for payment in filtered_payments:
+#         result = [
+#             {
+#                 "id": payment.id,
+#                 "order_id": payment.order_id,
+#                 "payment_method": payment.payment_method,
+#                 "amount": payment.amount,
+#                 # "payment_date": payment.payment_date.strftime('%Y-%m-%d')
+#             }
+#         ]
 
-    return jsonify(result)
+#     return jsonify(result)
 
 @blueprint.route('/payment', methods=["GET"])
 def payment():
-    search_payment()
-    p = Payment.query.all()
-    all_payment = []
-    for payment_in_for in p:
-        all_payment.append({
-            "id":payment_in_for.id,
-            'order_id':payment_in_for.order_id,
-            'payment_method':payment_in_for.payment_method,
-            'amount':payment_in_for.amount,
-            'payment_date':payment_in_for.payment_date,
-        })
+    try:
+        # Parse the search filter from the request arguments
+        search = ast.literal_eval(request.args.get("filter"))
+        if not search:
+            p = Payment.query.all()
+        else:
+            # Query the Payment table based on the search filter
+            p = Payment.query.filter(
+                (Payment.order_id.ilike(f'%{search["name"]}%')) |
+                (Payment.payment_method.ilike(f'%{search["name"]}%')) |
+                (Payment.payment_date.ilike(f'%{search["name"]}%'))
+            ).all()
 
+        # Create a list to store the payment data
+        all_payment = []
+        for payment_in_for in p:
+            all_payment.append({
+                "id": payment_in_for.id,
+                'order_id': payment_in_for.order_id,
+                'payment_method': payment_in_for.payment_method,
+                'amount': payment_in_for.amount,
+                'payment_date': payment_in_for.payment_date,
+            })
 
-    response = jsonify(all_payment)
-    response.headers['Access-Control-Expose-Headers'] = 'Content-Range'
-    response.headers['Content-Range'] = len(all_payment)
-    return response
+        # Create a JSON response with the payment data
+        response = jsonify(all_payment)
+        response.headers['Access-Control-Expose-Headers'] = 'Content-Range'
+        response.headers['Content-Range'] = str(len(all_payment))
+        return response
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 
 @blueprint.route('/payment/<int:id>', methods=["GET"])
