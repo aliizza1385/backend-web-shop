14740cbc5683926c20959b2f2bbed4b7715fb6ad
alireza <aliizza1385@gmail.com>
2024-04-14 12:33:40 -0700

    I ended project

--- customer_viwe/route.py
+++ customer_viwe/route.py
@@ -1,4 +1,3 @@
-from customer_viwe.main.route import blueprint as customer_viwe
 from customer_viwe.login.route import blueprint as login
 from customer_viwe.shop.route import blueprint as shop
 from customer_viwe.customer.route import blueprint as customer
